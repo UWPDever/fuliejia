5e6260ede68aac70032add44b500be7255e81f1f
shaosmpet@163.com <shaosmpet@163.com>
2019-06-30 16:45:07 +0800

    init

---
+++ dist/static/mynet_files/check.js
@@ -0,0 +1,314 @@
+﻿
+function CX() {
+    var P_Num = document.getElementById("P_Num").value;
+    var V_Code = document.getElementById("V_Code").value;
+    if (P_Num.trim() == "") { document.getElementById("MSG").innerHTML = "*产品数码不能为空"; return; }
+    if (P_Num.length != 16 || !checkNum(document.getElementById("P_Num"))) { document.getElementById("MSG").innerHTML = "*请输入16位纯数字数码"; return; }
+    if (V_Code.trim() == "") { document.getElementById("MSG").innerHTML = "*验证码不能为空"; return; }
+    if (!checkNum(document.getElementById("V_Code"))) { document.getElementById("MSG").innerHTML = "*请正确输入数字验证码"; return; }
+    if ($("#Mobile").length > 1) {
+        if (!isPhone(document.getElementById("Mobile"))) { document.getElementById("MSG").innerHTML = "*请输入正确的手机号码"; return; }
+    }
+    document.getElementById("MSG").innerHTML = "";
+    var mobile = "";
+    if ($("#Mobile").length > 1) {
+        mobile = $("#Mobile").val();
+    }
+    //var mobile = document.getElementById("Mobile").value;
+    //CheckUserExit(P_Num, V_Code, mobile);
+    document.getElementById("MSG").innerHTML = "<span style='color:green;'>正在查询，请您稍后...</span>";
+    $.ajax({
+        type: 'POST',
+        url: '../Ajax/Check.ashx',
+        data: 'code=' + P_Num + '&imgcode=' + V_Code + "&mobile=" + mobile + "&t=" + Math.random(),
+        async: false,
+        success: function (data) {
+            if (data == '1') {
+                document.getElementById("MSG").innerHTML = "*验证码错误"; document.getElementById("V_Code").value = "";
+                document.getElementById("img_Code").src = document.getElementById("img_Code").src + "&";
+                return;
+            }
+            else if (data == "2") {
+                document.getElementById("MSG").innerHTML = "*网络繁忙，请稍候再试";
+                return;
+            }
+            else {
+                var arr = data.split('|');
+                if (arr.length > 1 && arr[0] == "003") {
+                    data = arr[1];
+                    if ($("#linkClt").length > 0) {
+                        $("#linkClt").show();
+                        var url = $("#linkClt").attr("href");
+                        var urlarr = url.split('&code=');
+                        if (urlarr.length > 1) {
+                            if (urlarr[1].indexOf("&") == -1) {
+                                url = urlarr[0];
+                            }
+                            else {
+                                url = urlarr[0] + "&" + urlarr[1].split('&')[1];
+                            }
+                        }
+                        $("#linkClt").attr("href", url.replace("&url=", "&code=" + P_Num + "&url="));
+                    }
+                    else {
+                        $("#linkClt").hide();
+                    }
+                }
+                document.getElementById("result").innerHTML = data;
+                document.getElementById("div_cx").style.display = "none";
+                document.getElementById("div_result").style.display = "block";
+                return;
+            }
+
+        },
+        error: function (e) {
+            document.getElementById("MSG").innerHTML = "*查询失败，请稍候再试！";
+            return;
+        }
+
+    });
+
+};
+
+
+function CXNoValidCode() {
+    var P_Num = document.getElementById("P_Num").value;
+    //var V_Code = document.getElementById("V_Code").value;
+    if (P_Num.trim() == "") { document.getElementById("MSG").innerHTML = "*产品数码不能为空"; return; }
+    if (P_Num.length != 16 || !checkNum(document.getElementById("P_Num"))) { document.getElementById("MSG").innerHTML = "*请输入16位纯数字数码"; return; }
+    //if (V_Code.trim() == "") { document.getElementById("MSG").innerHTML = "*验证码不能为空"; return; }
+    //if (!checkNum(document.getElementById("V_Code"))) { document.getElementById("MSG").innerHTML = "*请正确输入数字验证码"; return; }
+    if ($("#Mobile").length > 1) {
+        if (!isPhone(document.getElementById("Mobile"))) { document.getElementById("MSG").innerHTML = "*请输入正确的手机号码"; return; }
+    }
+    document.getElementById("MSG").innerHTML = "";
+    var mobile = "";
+    if ($("#Mobile").length > 1) {
+        mobile = $("#Mobile").val();
+    }
+    //var mobile = document.getElementById("Mobile").value;
+    //CheckUserExit(P_Num, V_Code, mobile);
+    document.getElementById("MSG").innerHTML = "<span style='color:green;'>正在查询，请您稍后...</span>";
+    $.ajax({
+        type: 'POST',
+        url: '../Ajax/Check.ashx',
+        data: 'code=' + P_Num + "&mobile=" + mobile + "&t=" + Math.random(),
+        async: false,
+        success: function (data) {
+            if (data == '1') {
+                document.getElementById("MSG").innerHTML = "*验证码错误"; document.getElementById("V_Code").value = "";
+                document.getElementById("img_Code").src = document.getElementById("img_Code").src + "&";
+                return;
+            }
+            else if (data == "2") {
+                document.getElementById("MSG").innerHTML = "*网络繁忙，请稍候再试";
+                return;
+            }
+            else {
+                var arr = data.split('|');
+                if (arr.length > 1 && arr[0] == "003") {
+                    data = arr[1];
+                    if ($("#linkClt").length > 0) {
+                        $("#linkClt").show();
+                        var url = $("#linkClt").attr("href");
+                        var urlarr = url.split('&code=');
+                        if (urlarr.length > 1) {
+                            if (urlarr[1].indexOf("&") == -1) {
+                                url = urlarr[0];
+                            }
+                            else {
+                                url = urlarr[0] + "&" + urlarr[1].split('&')[1];
+                            }
+                        }
+                        $("#linkClt").attr("href", url.replace("&url=", "&code=" + P_Num + "&url="));
+                    }
+                    else {
+                        $("#linkClt").hide();
+                    }
+                }
+                document.getElementById("result").innerHTML = data;
+                document.getElementById("div_cx").style.display = "none";
+                document.getElementById("div_result").style.display = "block";
+                return;
+            }
+
+        },
+        error: function (e) {
+            document.getElementById("MSG").innerHTML = "*查询失败，请稍候再试！";
+            return;
+        }
+
+    });
+
+};
+
+
+
+function CXShell() {
+    var P_Num = document.getElementById("P_Num").value;
+    var V_Code = document.getElementById("V_Code").value;
+    if (P_Num.trim() == "") { document.getElementById("MSG").innerHTML = "*AC Code cannot be empty"; return; }
+    if (P_Num.length != 16 || !checkNum(document.getElementById("P_Num"))) { document.getElementById("MSG").innerHTML = "*Please input 16 digits but text"; return; }
+    if (V_Code.trim() == "") { document.getElementById("MSG").innerHTML = "*Verification code cannot be empty"; return; }
+    if (!checkNum(document.getElementById("V_Code"))) { document.getElementById("MSG").innerHTML = "*Please input digital verification code"; return; }
+    if ($("#Mobile").length > 1) {
+        if (!isPhone(document.getElementById("Mobile"))) { document.getElementById("MSG").innerHTML = "*Please input correct telephone number"; return; }
+    }
+    document.getElementById("MSG").innerHTML = "";
+    var mobile = "";
+    if ($("#Mobile").length > 1) {
+        mobile = $("#Mobile").val();
+    }
+    //var mobile = document.getElementById("Mobile").value;
+    //CheckUserExit(P_Num, V_Code, mobile);
+    document.getElementById("MSG").innerHTML = "<span style='color:green;'>Submitting in process, please be patient...</span>";
+    $.ajax({
+        type: 'POST',
+        url: '../Ajax/Check.ashx',
+        data: 'code=' + P_Num + '&imgcode=' + V_Code + "&mobile=" + mobile + "&t=" + Math.random(),
+        async: false,
+        success: function (data) {
+            if (data == '1') {
+                document.getElementById("MSG").innerHTML = "*Verification Code is wrong"; document.getElementById("V_Code").value = "";
+                document.getElementById("img_Code").src = document.getElementById("img_Code").src + "&";
+                return;
+            }
+            else if (data == "2") {
+                document.getElementById("MSG").innerHTML = "*Network busy, please try again later";
+                return;
+            }
+            else {
+                var arr = data.split('|');
+                if (arr.length > 1 && arr[0] == "003") {
+                    data = arr[1];
+                    if ($("#linkClt").length > 0) {
+                        $("#linkClt").show();
+                        var url = $("#linkClt").attr("href");
+                        var urlarr = url.split('&code=');
+                        if (urlarr.length > 1) {
+                            if (urlarr[1].indexOf("&") == -1) {
+                                url = urlarr[0];
+                            }
+                            else {
+                                url = urlarr[0] + "&" + urlarr[1].split('&')[1];
+                            }
+                        }
+                        $("#linkClt").attr("href", url.replace("&url=", "&code=" + P_Num + "&url="));
+                    }
+                    else {
+                        $("#linkClt").hide();
+                    }
+                }
+                document.getElementById("result").innerHTML = data;
+                document.getElementById("div_cx").style.display = "none";
+                document.getElementById("div_result").style.display = "block";
+                return;
+            }
+
+        },
+        error: function (e) {
+            document.getElementById("MSG").innerHTML = "*Submitting Failed, please try again later！";
+            return;
+        }
+
+    });
+
+}
+function CZ() {
+    document.getElementById("P_Num").value = ""; document.getElementById("V_Code").value = "";
+    document.getElementById("img_Code").src = document.getElementById("img_Code").src + "&";
+    document.getElementById("MSG").innerHTML = "";
+}
+
+function BK() {
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("div_cx").style.display = "block";
+    document.getElementById("div_result").style.display = "none";
+    CZ();
+}
+
+//11位防伪码中转页面调用
+function CX1(obj) {
+    //将按钮禁用1.5秒，避免重复提交
+    $(obj).attr('disabled', "true");
+    setTimeout(function () {
+        $(obj).removeAttr("disabled");
+    }, 1500);
+    var tz = document.getElementById("hidTz").value;
+    var url = location.href;
+    var code = getQueryString("code");
+    url = url.replace("CheckPage", tz);
+    var P_Num = document.getElementById("P_Num").value;
+    if (P_Num.trim() == "") { document.getElementById("MSG").innerHTML = "*产品数码不能为空"; return; }
+    if (P_Num.length != 6 || !checkNum(document.getElementById("P_Num"))) { document.getElementById("MSG").innerHTML = "*请输入最后6位纯数字数码"; return; }
+    var tempCode = code + P_Num.substring(1);
+    url = url.replace(code, tempCode);
+    window.location.href = url;
+}
+//取得url参数的值
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+//验证码中转页面调用
+function ValidCode() {
+
+    var V_Code = document.getElementById("V_Code").value;
+    if (V_Code.trim() == "") { document.getElementById("MSG").innerHTML = "*请输入验证码"; return; }
+
+    $('#btnValid').attr('disabled', "true");
+    setTimeout(function () {
+        $('#btnValid').removeAttr("disabled");
+    }, 6000);
+
+    $.ajax({
+        type: 'POST',
+        url: '../Ajax/ValidCheck.ashx',
+        data: 'imgcode=' + V_Code + "&t=" + Math.random(),
+        async: false,
+        success: function (data) {
+            if (data == '1') {
+                document.getElementById("MSG").innerHTML = "*验证码错误"; document.getElementById("V_Code").value = "";
+                document.getElementById("img_Code").src = document.getElementById("img_Code").src + "&";
+                $('#btnValid').removeAttr("disabled");
+                return;
+            } else {
+                var tz = document.getElementById("hidTz").value;
+                var url = location.href;
+                var code = getQueryString("code");
+                //2015年1月22日 10:56:08 徐磊
+                var validName = url.substring(url.indexOf("Valid"), url.lastIndexOf("."));
+                //url = url.replace("Valid", tz);
+                url = url.replace(validName, tz);
+                window.location.href = url;
+            }
+        },
+        error: function (e) {
+            document.getElementById("MSG").innerHTML = "*查询失败，请稍候再试！";
+            $('#btnValid').removeAttr("disabled");
+            return;
+        }
+
+    });
+}
+//验证码中转页面调用
+function CZValid() {
+    document.getElementById("V_Code").value = "";
+    document.getElementById("img_Code").src = document.getElementById("img_Code").src + "&";
+    document.getElementById("MSG").innerHTML = "";
+}
+
+/***********选择语言页面调用****************/
+
+//选择语言页面调用
+function CheckLan() {
+    var tz = document.getElementById("hidTz").value;
+    var obj = document.getElementById("ddlLan");
+    var index = obj.selectedIndex;
+    var value = obj.options[index].value; // 选中值
+    var url = location.href;
+    url = url.replace("CheckLanguage", tz);
+    window.location.href = url + "&language=" + value;
+}
---
+++ dist/static/mynet_files/load.js
@@ -0,0 +1,190 @@
+﻿
+//正则表达式方法
+function checkNum(obj) {
+    var re = /^[0-9]*$/; //判断字符串是否为数字
+    if (!re.test(obj.value.trim())) {
+        //document.getElementById("MSG").innerHTML = "*请输入只能输入数字";
+        //obj.value = "";
+        obj.focus();
+        return false;
+    }
+    else {
+        if (obj.value.trim() != "") {
+            document.getElementById("MSG").innerHTML = "";
+        }
+        return true;
+    }
+}
+function IsZIP(str) {
+    if (str.length != 0) {
+        reg = /^\d{6}$/;
+        return reg.test(str);
+    }
+}
+//是否含有中文（也包含日文和韩文）   
+function isChineseChar(obj) {
+    //var reg = /^[\u4E00-\u9FA5\uF900-\uFA2D]*$/;
+    var reg = /[^\x00-\xff]/g;
+    if (reg.test(obj.value.trim())) {
+        //document.getElementById("MSG").innerHTML = "*输入错误,不能输入汉字";
+
+        //obj.value = "";
+        return false;
+    }
+    else {
+        if (obj.value.trim() != "") {
+            document.getElementById("MSG").innerHTML = "";
+        }
+        return true;
+    }
+}
+//同理，是否含有全角符号的函数
+function isFullwidthChar(obj) {
+    var reg = /[\uFF00-\uFFEF]/;
+    if (obj.value.trim() != "请输入" && !reg.test(obj.value.trim())) {
+        //document.getElementById("MSG").innerHTML = "*输入错误,不能输入全角符号";
+        //obj.value = "";
+        return false;
+    }
+    else {
+        if (obj.value.trim() != "") {
+            document.getElementById("MSG").innerHTML = "";
+        }
+        mBlur(obj);
+        return true;
+    }
+}
+
+function mFocus(obj) {
+    if (obj.value.trim() == "请输入") {
+        obj.value = "";
+    }
+    obj.style.color = "#000";
+}
+function mBlur(obj) {
+    if (obj.value.trim() == "") {
+        obj.value = "请输入";
+        obj.style.color = "#929292";
+    }
+}
+
+function isPhone(obj) {
+   // var reg = /(^[0-9]{3,4}\-[0-9]{7,8}$)|(^[0-9]{7,8}$)|(^0{0,1}1[345789][0-9]{9}$)/
+    var reg = /^0?1[3|4|5|6|7|8|9][0-9]\d{8}$/;
+    //return reg.test(str);
+    if (obj.value.trim() != "" && !reg.test(obj.value.trim())) {
+        //document.getElementById("MSG").innerHTML = "*输入错误,手机号格式错误";
+        //obj.value = "";
+        obj.focus();
+        return false;
+    }
+    else {
+        document.getElementById("MSG").innerHTML = "";
+        return true;
+    }
+
+}
+
+
+
+function isPhone1(str) {
+    //var reg = /(^[0-9]{3,4}\-[0-9]{7,8}$)|(^[0-9]{7,8}$)|(^0{0,1}1[358][0-9]{9}$)/
+   // var reg = /^0?1[3|4|5|7|8|9][0-9]\d{8}$/;
+      var reg = /^0?1[3|4|5|6|7|8|9][0-9]\d{8}$/;
+    return reg.test(str.trim());
+}
+
+function isEmail(str) {
+    var reg = /^([a-zA-Z0-9_-]{1,20}(\.?[a-zA-Z0-9_-]{2,3}){1,2})+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+    return reg.test(str.trim());
+}
+function isTelephone(str) {
+    var reg = /^(\d{3,4}\-)?[1-9]\d{6,7}$/;
+    return reg.test(str.trim());
+}
+
+//只能包含数字字母
+function NumOrStr(str) {
+    var reg = /^[0-9a-zA-Z]*$/;
+    return reg.test(str.trim());
+}
+//必须是数字字母组合
+function NumAndStr(str) {
+    //var reg = /^(([0-9]+[a-zA-Z]+[a-zA-Z0-9]+)|([a-zA-Z]+[0-9]+[a-zA-Z0-9]+))$/;
+    //var reg = /^(([a-zA-Z]{1}[0-9]+)|[0-9]{1}[a-zA-Z0-9]+)$/;
+    //2015年9月25日 10:01:48 徐磊修改
+    var reg = /(?=.*?[a-zA-Z])(?=.*?[0-9])[a-zA-Z0-9]{6,}$/;
+    return reg.test(str.trim());
+}
+
+//验证是否为数字
+function isNum(str) {
+    var reg = /^[0-9]*$/;
+    return reg.test(str.trim());
+}
+//验证邮编
+function isPostCode(str) {
+    var reg = /^[1-9][0-9]{5}$/
+    return reg.test(str.trim());
+
+}
+
+
+
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+function getUrlParamString(str) {
+    var r = window.location.search;
+    if (r.indexOf(str) != -1) {
+        return r.split(str)[1];
+    }
+    else {
+        return "";
+    }
+}
+
+
+String.prototype.trim = function () {
+    return this.replace(/(^\s*)|(\s*$)/g, "").replace(/\&/g, "");
+}
+
+
+
+
+//检测是否是Android手机， 是，则返回true    否 则返回false
+function CheckAndroid() {
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+
+    return isAndroid;
+    //    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    //    alert('是否是Android：' + isAndroid);
+    //    alert('是否是iOS：' + isiOS);
+
+}
+
+//检测是否是iOS手机打开 是，则返回true    否 则返回false
+function CheckIOS() {
+    var u = navigator.userAgent;
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    return isiOS;
+}
+
+//判断是否是微信浏览器打开
+function isWeiXin() {
+    var ua = window.navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+        return true;
+    } else {
+
+        return false;
+
+    }
+}
+
+
